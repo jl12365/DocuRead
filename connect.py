09c6cc17422b7788ce81125e1472c3675365969b
Jason <jasonli12365@gmail.com>
2024-11-27 14:49:44 -0500

    added function to upload document to s3

--- connect.py
+++ connect.py
@@ -1,3 +1,16 @@
 import boto3
 import os
 
+s3 = boto3.client('s3')
+
+bucket_name = 'ise391test'
+file_path = r'C:\Users\jason\projects\DocuRead\Documents\amazon filing 2.pdf'
+s3_key = 'uploads/documents/amazon_filing_2.pdf'
+
+try:
+    s3.upload_file(file_path, bucket_name, s3_key)
+    print("file was successfully uploaded to s3")
+except Exception as e:
+    print("an error occured: {e}")
+
+
