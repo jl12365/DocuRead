3d9f29f650e1b48c9825976ed32f48114d60b145
Jason <jasonli12365@gmail.com>
2024-11-30 14:33:31 -0500

    .

--- connect.py
+++ connect.py
@@ -4,8 +4,9 @@ import os
 s3 = boto3.client('s3')
 
 bucket_name = 'ise391test'
+#change file path to yoru own document you want to upload
 file_path = r'C:\Users\jason\projects\DocuRead\Documents\amazon filing 2.pdf'
-s3_key = 'uploads/documents/amazon_filing_2.pdf'
+s3_key = 'amazon_filing_2.pdf'
 
 try:
     s3.upload_file(file_path, bucket_name, s3_key)
